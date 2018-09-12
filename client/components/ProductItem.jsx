ad3f344702445f13a8556851e7ff5f6e62101926
emily <emilycoco@me.com>
2018-09-12 20:35:28 +1200

    styles headings

--- client/components/ProductItem.jsx
+++ client/components/ProductItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProductItem = (props) => {
   return (
     <tr className="row">
-      <td className="col-sm">{props.name}</td>
+      <td className="col-sm name">{props.name}</td>
       <td className="col-sm">${props.price.toFixed(2)}</td>
       <td><button type="button" className="btn btn-dark"
         onClick={() => props.addToCart(props.name, props.price)}>add to cart</button></td>
